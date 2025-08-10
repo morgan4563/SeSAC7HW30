b8c8ccc764125b42df271d9e1ee73e1d41265931
morgan4563 <morganpark4563@gmail.com>
2025-08-11 02:23:16 +0900

    feat: 닉네임 텍스트필드 검증로직 추가

--- MVVMBasic/ProfileSetting/ProfileSettingViewController.swift
+++ MVVMBasic/ProfileSetting/ProfileSettingViewController.swift
@@ -27,7 +27,29 @@ class ProfileSettingViewController: UIViewController {
         image.tintColor = .customBlue
         image.clipsToBounds = true
 
-		return image
+        return image
+    }()
+
+    let nickNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "닉네임을 입력해주세요 :)"
+        textField.font = .systemFont(ofSize: 15)
+
+        return textField
+    }()
+
+    let underLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+
+        return view
+    }()
+
+    let resultLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13)
+
+		return label
     }()
 
     override func viewDidLoad() {
@@ -56,8 +78,11 @@ class ProfileSettingViewController: UIViewController {
 
     func configureHierachy() {
         view.addSubview(profileButton)
-
         view.addSubview(profileCameraImage)
+
+        view.addSubview(nickNameTextField)
+        view.addSubview(underLine)
+        view.addSubview(resultLabel)
     }
 
     func configureLayout() {
@@ -66,13 +91,27 @@ class ProfileSettingViewController: UIViewController {
             make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             make.size.equalTo(88)
         }
-
         //TODO: 이미지 잘림, profileButton안에 넣으면 cliptoBounds로 잘림, 밖에두면 선이 겹침, 해결필요
         profileCameraImage.snp.makeConstraints { make in
             make.bottom.equalTo(profileButton.snp.bottom)
             make.trailing.equalTo(profileButton.snp.trailing)
             make.size.equalTo(32)
         }
+
+        nickNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileButton.snp.bottom).offset(32)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        underLine.snp.makeConstraints { make in
+            make.top.equalTo(nickNameTextField.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(32)
+            make.height.equalTo(1)
+        }
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalTo(underLine.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+
     }
 
     func configureView() {
@@ -81,10 +120,42 @@ class ProfileSettingViewController: UIViewController {
         }
 
         profileButton.addTarget(self, action: #selector(profileButtonTapped), for: .touchUpInside)
+        nickNameTextField.addTarget(self, action: #selector(didChangeTextField(_:)), for: .editingChanged)
     }
 
     @objc func profileButtonTapped() {
 	//TODO: 프로필 이미지 세팅 뷰로 넘어가기
         print("클릭됨")
+    }
+
+    //TODO: do catch
+    @objc func didChangeTextField(_ textField: UITextField) {
+        guard let nickname = textField.text else {
+            resultLabel.text = ""
+            return
+        }
+
+        guard nickname.count > 1 && nickname.count < 10 else {
+            resultLabel.text = "닉네임은 2글자 이상, 10글자 미만으로 입력해주세요"
+            resultLabel.textColor = .red
+            return
+        }
+
+        let spacialSignRegex = "^[^#$@%]+$"
+        guard (nickname.range(of: spacialSignRegex, options: .regularExpression) != nil) else {
+            resultLabel.text = "닉네임에 @, #, $, % 는 포함할 수 없어요"
+            resultLabel.textColor = .red
+            return
+        }
+
+        let numberRegex = "^[^0-9]+$"
+        guard (nickname.range(of: numberRegex, options: .regularExpression) != nil) else {
+            resultLabel.text = "닉네임에 숫자는 포함할 수 없어요"
+            resultLabel.textColor = .red
+            return
+        }
+
+        resultLabel.text = "사용할 수 있는 닉네임이에요"
+        resultLabel.textColor = .blue
     }
 }
